21c4bb5e101a039dcb395c4331250dc46a2764a8
Nikhil <nikhil@antarx.tech>
2018-11-13 03:06:57 +0530

    audio is working

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require('express');
-const app     = express();
-const port    = 3000;
+const express     = require('express');
+const path        = require('path');
+const app         = express();
+const port        = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.send ('Hello World!'));
+app.get('/receive', (req, res) => res.send ('Hello World!'));
+app.get('/transmit', (req, res) => { 
+	res.render ('demo', {});
+});
 
 app.listen(port, () => console.log (`Example app listening on port ${port}!`));
